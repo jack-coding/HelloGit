77bb9460b4c00a160b7557c52512af0731b2f776
jack <2515906731@qq.com>
2021-03-10 21:47:12 +0800

    添加文件

---
+++ helloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author: 钟静
+ * @Date: 2021/3/10 21:29
+ * @Description:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
